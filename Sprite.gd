ba7d89e5d43b55266f7bcec1262c84e08bcfe88a
raimon <101257136+raimonlopezg@users.noreply.github.com>
2022-03-14 14:27:56 +0100

    es mou WASD
    el personatge es mou segons les tecles

--- Sprite.gd
+++ Sprite.gd
@@ -6,6 +6,7 @@ extends Sprite
 # var b = "text"
 export var velocitat = Vector2 (-250,-180)
 export var velocitat_deg = (-180)
+var direccio = Vector2(0,0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position = Vector2(450,330)
@@ -13,14 +14,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += velocitat * delta
-	rotation_degrees += velocitat_deg * delta
+	direccio = Vector2.ZERO
+	if Input.is_action_pressed("mou dreta"):
+		direccio += Vector2.LEFT 
+	if Input.is_action_pressed("mou esquerra"):
+		direccio += Vector2.RIGHT
+	if Input.is_action_pressed("mou adalt"):
+		direccio += Vector2.DOWN
+	if Input.is_action_pressed("mou abaix"):
+		direccio += Vector2.UP 
 	
-	if position.x >= 1024 or position.x <= 0 :
-		velocitat.x *= -1
-	if position.y >= 600 :
-		position.y = 600
-		velocitat.y *= -1
-	if position.y <= 0:
-		position.y = 0
-		velocitat.y *= -1
+		
+	position += direccio.normalized() * velocitat * delta
