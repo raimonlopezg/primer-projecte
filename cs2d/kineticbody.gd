b76f8a82df1d6efe30be4188a13ead748650d8a7
raimon <101257136+raimonlopezg@users.noreply.github.com>
2022-03-21 14:23:08 +0100

    mario bros
    he fet que sembi mario bros (una mica)

--- cs2d/kineticbody.gd
+++ cs2d/kineticbody.gd
@@ -2,8 +2,21 @@ extends KinematicBody2D
 
 var velocitat_base = 200
 var velocitat = Vector2.ZERO
-var direccio = Vector2.DOWN
+var velocitat_de_salt = -550
+
 var gravetat = Vector2.DOWN*980
+
 func _physics_process(delta):
-	move_and_slide(velocitat*direccio)
+	velocitat.x = 0
 	velocitat += gravetat*delta
+	if Input.is_action_pressed("mou dreta"):
+		velocitat += Vector2.RIGHT * velocitat_base
+	if Input.is_action_pressed ("mou esquerra"):
+		velocitat += Vector2.LEFT * velocitat_base
+		
+	if Input.is_action_just_pressed("mou adalt") and is_on_floor():
+		velocitat.y = velocitat_de_salt
+	if Input.is_action_just_pressed("mou adalt") and is_on_wall():
+		velocitat.y = velocitat_de_salt
+		
+	velocitat = move_and_slide(velocitat, Vector2.UP)
